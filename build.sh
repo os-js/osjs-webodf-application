2e14825a7463bb3c3dfff80bb30d38a83c5416bc
Anders Evenrud <andersevenrud@gmail.com>
2018-12-23 02:34:19 +0100

    Added WebODF submodule and build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+npm run eslint
+npm run stylelint
+
+mkdir -p dist
+
+rm -rf dist/*
+rm -rf build
+
+git submodule update --remote
+
+mkdir -p build
+pushd build
+  cmake ../WebODF
+  make webodf.js-target
+popd
+
+NODE_ENV=production npm run build
